f07bcfd6eb6c8ee7810407acfedc51937a8bc486
Geordie Mac <Joshua.McManus2006@gmail.com>
2026-08-04 15:39:42 +0100

    bugs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-import AutoImport from "astro-auto-import";
 import { defineConfig } from "astro/config";
 import remarkCollapse from "remark-collapse";
 import remarkToc from "remark-toc";
@@ -19,24 +17,24 @@ export default defineConfig({
     prefetchAll: true
   },
   adapter: cloudflare(),
-  integrations: [react(), sitemap(), tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), AutoImport({
-    imports: ["@components/common/Button.astro", "@shortcodes/Accordion", "@shortcodes/Notice", "@shortcodes/Youtube", "@shortcodes/Tabs", "@shortcodes/Tab"]
-  }), mdx()],
+  integrations: [
+    react(),
+    sitemap(),
+    mdx({
+      remarkPlugins: [
+        remarkToc,
+        [remarkCollapse, { test: "Table of contents" }],
+        remarkMath
+      ],
+      rehypePlugins: [[rehypeKatex, {}]]
+    })
+  ],
   markdown: {
-    remarkPlugins: [remarkToc, [remarkCollapse, {
-      test: "Table of contents"
-    }], remarkMath],
-    rehypePlugins: [[rehypeKatex, {}]],
     shikiConfig: {
-      themes: { // https://shiki.style/themes
+      themes: {
         light: "light-plus",
         dark: "dark-plus",
       } 
-    },
-    extendDefaultPlugins: true
+    }
   },
 });
