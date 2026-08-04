9d8d2a8b0939d74befd452407a3ecaa8927340dc
Geordie Mac <Joshua.McManus2006@gmail.com>
2026-08-04 15:59:53 +0100

    fix: resolve build config and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import mdx from "@astrojs/mdx";
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 import sitemap from "@astrojs/sitemap";
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 import remarkCollapse from "remark-collapse";
 import remarkToc from "remark-toc";
 import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
-import cloudflare from "@astrojs/cloudflare";
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
